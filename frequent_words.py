4951e7b8cf161563325809722fb14f8f520845c0
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2024-07-21 18:57:28 -0500

    Update frequent_words.py

--- frequent_words.py
+++ frequent_words.py
@@ -1,16 +1,5 @@
-from maxmap import maxMap
+from bio_lib import maxMap, frequencyTable
 import sys
-
-def frequencyTable(text: str, k: int):
-    freqMap = {}
-    for i in range(len(text) - k + 1):
-        subText = text[i:i+k]
-        if subText in freqMap.keys():
-            freqMap[subText] += 1
-        else:
-            freqMap[subText] = 1
-    return freqMap
-
 
 def freqWords(text: str, k: int):    
     freqMap = frequencyTable(text, k)
