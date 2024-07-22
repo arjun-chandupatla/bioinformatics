ae44020b6618ff45b1d3457214c7ad113cee9d44
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2024-07-21 19:00:11 -0500

    Update approx_pattern_count.py

--- approx_pattern_count.py
+++ approx_pattern_count.py
@@ -2,4 +2,3 @@ from approx_pattern_match import approximate_pattern_matching
 
 def approx_pattern_count(text: str, pattern: str, d: int) -> int:
     return len(approximate_pattern_matching(pattern, text, d))
-
