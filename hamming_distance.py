00cbd4870852453c14118b1d136b71d3786f0ba2
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2024-07-23 19:50:53 -0500

    Update hamming_distance.py
    Raises an exception when strings are different lengths

--- hamming_distance.py
+++ hamming_distance.py
@@ -1,9 +1,8 @@
 def hamming_distance(p: str, q: str) -> int:
+    if len(p) != len(q):
+        raise Exception("Strings must be the same length")
     dist = 0
     for i in range(len(p)):
         if p[i] != q[i]:
             dist += 1
     return dist
-
-
-print(hamming_distance("AAAAA", "TTTTA"))
