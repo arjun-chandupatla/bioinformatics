7552b30a10da33e3e900e48a7aa46786777b38e3
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2024-07-23 20:12:54 -0500

    Added reverse complement functionality

--- frequent_words.py
+++ frequent_words.py
@@ -41,3 +41,23 @@ def frequent_words_with_mismatches(text: str, k: int, d: int) -> list[str]:
         if freqMap[p] == m:
             patterns.append(p)
     return patterns
+
+
+def frequent_words_mismatches_reverse_complement(text: str, k: int, d: int) -> list[str]:
+    patterns = []
+    freqMap = {}
+    n = len(text)
+    for i in range(n - k + 1):
+        pattern = text[i:i+k]
+        rc = reverseComplement(pattern)
+        neighborhood = neighbors(pattern) + neighbors(rc)
+        for neighbor in neighborhood:
+            if neighbor in freqMap:
+                freqMap[neighbor] += 1
+            else:
+                freqMap[neighbor] = 1
+    m = maxMap(freqMap)
+    for p in freqMap.keys():
+        if freqMap[p] == m:
+            patterns.append(p)
+    return patterns
