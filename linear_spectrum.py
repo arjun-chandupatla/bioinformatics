ed261b97a9c4ec9c163b7ba0eedc29bb26f12a2f
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-04-13 16:44:37 -0500

    Update linear_spectrum.py

--- linear_spectrum.py
+++ linear_spectrum.py
@@ -1,6 +1,6 @@
 # return the mass spectrum of a linear peptide
 
-def peptide_mass(pep: str) -> int:
+def peptide_mass(pep: str) -> int:    # find the  mass of a peptide
     AminoAcidMass = {'G': 57, 'A': 71, 'S': 87, 'P': 97, 'V': 99, 'T': 101, 'C': 103, 'I': 113,
                  'L': 113, 'N': 114, 'D': 115, 'K': 128, 'Q': 128, 'E': 129, 'M': 131,
                  'H': 137, 'F': 147, 'R': 156, 'Y': 163, 'W': 186, '': 0}
