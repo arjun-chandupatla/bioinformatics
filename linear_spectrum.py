d45b044d4a291f182eccdeef8b2330ffb1d7674b
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-04-13 16:43:34 -0500

    Create linear_spectrum.py

---
+++ linear_spectrum.py
@@ -0,0 +1,25 @@
+# return the mass spectrum of a linear peptide
+
+def peptide_mass(pep: str) -> int:
+    AminoAcidMass = {'G': 57, 'A': 71, 'S': 87, 'P': 97, 'V': 99, 'T': 101, 'C': 103, 'I': 113,
+                 'L': 113, 'N': 114, 'D': 115, 'K': 128, 'Q': 128, 'E': 129, 'M': 131,
+                 'H': 137, 'F': 147, 'R': 156, 'Y': 163, 'W': 186, '': 0}
+    m = 0
+    for a in pep.upper():
+        m += AminoAcidMass[a]
+    return m
+
+
+def prefix_mass(pep: str):
+    for i in range(len(pep) + 1):
+        yield peptide_mass(pep[:i])
+
+
+def linear_spectrum(pep: str) -> list[int]:
+    pmass = list(prefix_mass(pep))
+    spectrum = [0]
+    for i in range(len(pep)):
+        for j in range(i+1, len(pep) + 1):
+            spectrum.append(pmass[j] - pmass[i])
+    spectrum.sort()
+    return spectrum
