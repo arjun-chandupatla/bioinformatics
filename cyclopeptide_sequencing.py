7ea65a5f6d3ddc83ca3d2163699dff2dc9e4d119
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-04-21 17:19:52 -0500

    Update cyclopeptide_sequencing.py

--- cyclopeptide_sequencing.py
+++ cyclopeptide_sequencing.py
@@ -1,3 +1,14 @@
+def peptide_mass(pep: str) -> int:
+    AminoAcidMass = {'G': 57, 'A': 71, 'S': 87, 'P': 97, 'V': 99, 'T': 101, 'C': 103, 'I': 113,
+                 'L': 113, 'N': 114, 'D': 115, 'K': 128, 'Q': 128, 'E': 129, 'M': 131,
+                 'H': 137, 'F': 147, 'R': 156, 'Y': 163, 'W': 186, '': 0}
+    m = 0
+    for a in pep.upper():
+        m += AminoAcidMass[a]
+    return m
+
+
+
 def cyclopeptide_sequencing(spectrum: list[int]) -> list[list[int]]:
     candidates = [""]   # candidate peptides
     final = []          # final peptides
